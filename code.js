dbf2d360b7175bbf21b14f2c824e07d7b4ca4360
Sayantan Das <89656888+LetsBeTechCreators@users.noreply.github.com>
2021-12-07 21:27:38 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,6 @@
+onEvent("Geometric", "click", function( ) {
+  setProperty("screen1", "image", "https://www.thisiscolossal.com/wp-content/uploads/2014/06/2.gif");
+});
+onEvent("Gond", "click", function( ) {
+  setProperty("screen1", "image", "https://i.pinimg.com/originals/de/f2/c3/def2c3a61b6c1b980e444d9c6e4c3f6c.gif");
+});
